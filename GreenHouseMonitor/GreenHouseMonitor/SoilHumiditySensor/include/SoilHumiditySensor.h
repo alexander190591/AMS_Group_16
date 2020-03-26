3f3bfbf0964ecf6c74f9bcb0159e5797e626d1d4
alexander190591 <alexander190591@gmail.com>
2020-03-26 10:47:23 +0100

    Baby steps in Soil Moisture ADC driver...

--- GreenHouseMonitor/GreenHouseMonitor/SoilHumiditySensor/include/SoilHumiditySensor.h
+++ GreenHouseMonitor/GreenHouseMonitor/SoilHumiditySensor/include/SoilHumiditySensor.h
@@ -9,8 +9,15 @@
 #ifndef SOILHUMIDITYSENSOR_H_
 #define SOILHUMIDITYSENSOR_H_
 
+struct SoilHumiditySensor
+{
+	int humidityValue = 0;
+	unsigned char analogPin = 0;
+	double humidityInPercent = 0;
+};
 
-
-
+void SoilHumiditySensor_SetUp(unsigned char analogPin);
+unsigned int SoilHumiditySensor_AnalogRead();
+double SoilHumiditySensor_GetHumidityInPercent();
 
 #endif /* SOILHUMIDITYSENSOR_H_ */
